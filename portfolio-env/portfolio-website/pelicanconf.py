8c0317cdf12814ea92f1990f4695e88af31f47cd
Karthikeya Racharla <54552679+KarthikeyaR@users.noreply.github.com>
2025-02-02 13:41:12 +0530

    Initial website commit

---
+++ portfolio-env/portfolio-website/pelicanconf.py
@@ -0,0 +1,28 @@
+AUTHOR = 'Karthikeya'
+SITENAME = 'Karthikeya | AI & Data Science Professional'
+SITEURL = 'https://karthikeya.github.io/'
+
+# Path Configuration
+PATH = 'content'
+OUTPUT_PATH = 'output'
+THEME = 'themes/minimal'
+
+# Menu Items
+MENUITEMS = (
+    ('Home', '/'),
+    ('Projects', '/projects/'),
+    ('Publications', '/publications/'),
+    ('Blog', '/blog/'),
+    ('Contact', '/contact/'),
+)
+
+# Social widget
+SOCIAL = (
+    ('LinkedIn', 'https://www.linkedin.com/in/karthikeyaracharla'),
+    ('GitHub', 'https://github.com/KarthikeyaR/'),
+    ('Twitter', 'https://x.com'),
+)
+
+# Static Paths
+STATIC_PATHS = ['images', 'pdfs', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}} 
